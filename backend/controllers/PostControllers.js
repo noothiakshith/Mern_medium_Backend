d36a6096cfe251f6216af5ac267a4dae39f8e798
NOOTHI AKSHITH <125175973+noothiakshith@users.noreply.github.com>
2024-04-01 16:01:54 +0000

    Add update, read, readall functions to PostControllers.js

--- backend/controllers/PostControllers.js
+++ backend/controllers/PostControllers.js
@@ -21,10 +21,95 @@ const create = async(req,res)=>{
             title:title,
             content:content
         })
+        Post.update({published:true});
         await post.save();
         res.status(200).send('Post created');
     }
     catch(err){
         return res.status(400).send('Error');
     }
+}
+
+const update = async(req,res)=>{
+    const {id} = req.headers.id;
+    if(!id){
+        return res.status(400).send('Invalid input');
+    }
+    const check = isloggedin();
+    if(!check){
+        return res.status(400).send('User not logged in');
+    }
+    const finding = await Post.findOne({id:id});
+    if(!finding){
+        return res.status(400).send('Post not found');
+    }
+    try{
+        const{title,content} = postvalidation.safeparse(req.body);
+        if(!title||!content){
+            return res.status(400).send('Invalid input');
+        }
+        const updation = await Post.findOneAndUpdate({
+            title:title,
+            content:content
+        })
+         await updation.save();
+        res.status(200).send('Post updated');
+    }
+    catch(err){
+        return res.status(400).send('Error');
+    }
+}
+
+const read = async(req,res)=>{
+    const{id}=req.headers.id;
+    if(!id){
+        return res.status(400).send('Invalid input');
+    }
+    const checking = isloggedin();
+    if(!checking){
+        return res.status(400).send('User not logged in');
+    }
+    const finding = await Post.findOne({id:id});
+    if(!finding){
+        return res.status(400).send('Post not found');
+    }
+    try{
+        res.send(finding);
+    }
+    catch(err){
+        return res.status(400).send('Error');
+    }
+}
+
+const readall = async(req,res)=>{
+    const checking = isloggedin();
+    if(!checking){
+        return res.status(400).send('User not logged in');
+    }
+    try{
+        const finding = await Post.find();
+        res.send(finding);
+    }
+    catch(err){
+        return res.status(400).send('Error');
+    }
+}
+
+const isloggedin = ()=>{
+    const token = req.headers.token;
+    if(!token){
+        return false;
+    }
+    try{
+        const verified = jwt.verify(token,process.env.SECRET);
+        return true;
+    }
+    catch(err){
+        return false;
+    }
+}
+
+const generateid = ()=>{
+    const id = Math.floor(Math.random()*100000);
+    return id;
 }
