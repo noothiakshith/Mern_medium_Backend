4b1daaef8ee85d5ea087617b414c3c0bcde0ee7b
NOOTHI AKSHITH <125175973+noothiakshith@users.noreply.github.com>
2024-04-01 15:30:02 +0000

    Refactor UserControllers.js for signup and signin functionality

--- backend/controllers/UserControllers.js
+++ backend/controllers/UserControllers.js
@@ -1,11 +1,60 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const { uservalidation } = require('../zod/Validation');
-const { postvalidation } = require('../zod/Validation');
-signup,signin
+
 const signup = async(req,res)=>{
     const {email,password} = uservalidation.safeParse(req.body);
     if(!email||!password){
         return res.status(400).send('Invalid input');
     }
+    try{
+        const checking = await User.findOne({email:email});
+        if(checking){
+            return res.status(400).send('User already exists');
+        }
+        else{
+            newtokengeneration();
+            const user = new User({
+                email:email,
+                password:password
+            })
+            await user.save();
+            res.status(200).send('User created');
+        }
+    }
+    catch(err){
+        return res.status(400).send('Error');
+    }
 }
+
+const signin = async(req,res)=>{
+    const{email,password} = await uservalidation.safeParse(req.body);
+    if(!email||!password){
+        return res.status(400).send('Invalid input');
+    }
+    try{
+        const checking = await User.finOne({emaai:email});
+        if(!checking){
+            return res.status(400).send('User does not exist');
+        }
+        else{
+            if(checking.password!==password){
+                return res.status(400).send('Invalid password');
+            }
+            else{
+                newtokengeneration();
+                res.status(200).send('User logged in');
+            }
+        }
+    }
+    catch(err){
+        return res.status(400).send('Error');
+    }
+}
+
+const newtokengeneration = async()=>{
+    const token = jwt.sign({_id:User._id},process.env.TOKEN_SECRET);
+    return token;
+}
+
+module.exports={signin,signup,newtokengeneration}
